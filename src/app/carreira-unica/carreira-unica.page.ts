507aa0d16138132146743ca5dad43f434e30c2dc
pbgv2705 <paulaherbal@gmail.com>
2021-02-01 12:15:13 -0300

    Subindo Projeto Ionic

---
+++ src/app/carreira-unica/carreira-unica.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carreira-unica',
+  templateUrl: './carreira-unica.page.html',
+  styleUrls: ['./carreira-unica.page.scss'],
+})
+export class CarreiraUnicaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
