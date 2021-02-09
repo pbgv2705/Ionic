443cadc4d713522ec822ec8ddd3104a41a3d7d04
pbgv2705 <paulaherbal@gmail.com>
2021-02-09 14:39:50 -0300

    Ajuste de telas

--- src/app/carreira-unica/carreira-unica-routing.module.ts
+++ src/app/carreira-unica/carreira-unica-routing.module.ts
@@ -6,8 +6,19 @@ import { CarreiraUnicaPage } from './carreira-unica.page';
 const routes: Routes = [
   {
     path: '',
-    component: CarreiraUnicaPage
-  }
+    component: CarreiraUnicaPage,
+    children: [
+      {
+        path:'carreira-unica',
+        loadChildren: () => import('../carreira-unica/carreira-unica.module').then(m=>m.CarreiraUnicaPageModule)
+      }
+    ]
+  },
+  {
+        path:'',
+        redirectTo: '/tabs/tabs1',
+        pathMatch: 'full'
+      }
 ];
 
 @NgModule({
